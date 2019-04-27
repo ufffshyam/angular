6c95e8cd0b1db3cd921db5b1bab0fba49645997a
ufffshyam <gupta.shyam98.sg@gmail.com>
2019-04-27 13:07:27 +0530

    'loginpage'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+formdata;
+passwd;
+emailid;
+ constructor() { }
+  ngOnInit() {
+  this.formdata = new FormGroup({
+         emailid: new FormControl("angular@gmail.com"),
+         passwd: new FormControl("abcd1234")
+      });
+   }
+     onClickSubmit(data)
+{
+this.emailid=data.emailid;
+this.passwd=data.passwd;
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,9 +10,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+
+ }
